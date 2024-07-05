aeead5681d60d7294f6ce759ea40aebc7e6b4fc4
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-05 21:57:30 +0530

    Python

---
+++ boolen.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the no.1: "))
+b = int(input("Enter the no.2: "))
+
+if (a>b):
+    {
+        print(a,"is greater than",b)
+    }
+else:
+    {
+        print(b,"is greater than",a)  
+    }
