3baac18f82ee8a05c89e9f2a17a688cb00e5a9f2
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-08 00:31:37 +0530

    Python

--- loop.py
+++ loop.py
@@ -1,4 +1,12 @@
-for x in range(10):
-  print(x)
-else:
-  print("Finally done...!")
+# for x in range(10):
+#   print(x)
+# else:
+#   print("Finally done...!")
+
+n = int(input("Enter the number: "))
+fact=1
+i=1
+while i <= n:
+    fact*=i 
+    i+=1
+print("Factorial of ",n," is ",fact)
