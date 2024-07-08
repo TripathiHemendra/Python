94fbf476946eb0fbfb9710ebbe5cfe9c44029905
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-08 09:47:52 +0530

    Python

--- function.py
+++ function.py
@@ -1,4 +1,13 @@
-def my_function(fname, lname):
-  print(fname + " " + lname)
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
 
-my_function("Emil", "Refsnes")
+# my_function("Emil", "Refsnes")
+
+def odd_even(n):
+    if n%2==0:
+        print("Even")
+    else:
+        print("odd")
+        
+a=int(input("enter the number: "))
+odd_even(a)
