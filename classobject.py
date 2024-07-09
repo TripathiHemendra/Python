72994540c48cde2406e1dc6b2087e55167fe46d9
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-09 20:08:01 +0530

    Python

---
+++ classobject.py
@@ -0,0 +1,15 @@
+
+ 
+class student:
+    collage_name="PVG college"
+
+    def __init__(self,name,fees):
+        self.name=name
+        self.fees=fees
+        print("I am don")
+
+s1 = student("Himanshu","40000")
+print(s1.name)
+print(s1.fees)
+print(s1.collage_name)    
+        
--- classs.py
+++
@@ -1,8 +0,0 @@
-
- 
-class Employee:
-    Company="Google"
-    def getSalary(self):
-       print("The salary is 1000000")
-       himanshu = Employee()
-       himanshu.getSalary()
