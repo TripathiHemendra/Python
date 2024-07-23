0cce68e48eb472cab28753f6265d3f494a64fc30
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-23 10:11:30 +0530

    Python

--- practice/nested_loop.py
+++ practice/nested_loop.py
@@ -3,3 +3,5 @@ fruits = ["apple", "banana", "cherry"]
 for x in adj:
      for y in fruits: 
         print(x, y)
+
+
---
+++ practice/string_method.py
@@ -0,0 +1,6 @@
+a="HI my name is himanshu tripathi"
+print(len(a))
+
+a="Himanshu"
+b="Himanshu"
+print(a==b)
