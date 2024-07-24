0ab428334e06bd9408091c510f6f6b18cb16aa4f
TripathiHemendra <himanshutripathi858@gmail.com>
2024-07-24 10:49:01 +0530

    Python

--- practice/condition.py
+++ practice/condition.py
@@ -1,3 +1,11 @@
+#Odd_Even
+
+num=int(input("Enter the number: "))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
+
 #Visible for vote or not
 
 num=int(input("Enter the number: "))
@@ -20,5 +28,15 @@ else:
 #LAst Digit
 
 num=int(input("Enter the number: "))
-a=num%10
+a=num%10 
 print(a)
+
+
+#LAst Digit divisible by 3
+
+num=int(input("Enter the number: "))
+a=num%10 
+if a%3==0:
+    print(f"Yes the last degit of {num} is divisible by 3")
+else:
+    print(f"The last degit of {num} is not divisible by 3")
