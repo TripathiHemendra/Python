0022e90af91d476eeb515410359dcd4f9adfcc24
TripathiHemendra <himanshutripathi858@gmail.com>
2024-08-06 17:06:55 +0530

    Python

---
+++ function/first.py
@@ -0,0 +1,5 @@
+def hello(a):
+    print(a)
+    print("Kavya")
+    print("shraddha")
+hello("annu")
--- set/Cars.py
+++ set/Cars.py
@@ -1,3 +1,8 @@
-def car():
-    print('A car')
+def car(a,b):
+    c=a+b
+    return c
+h=8
+i=9
+x=car(h,i)
+print(x)
 
